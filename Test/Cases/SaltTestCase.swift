dc398919739988dc0e0cb0a936c89d2d97d4b518
Johannes Auer <jmjauer@gmail.com>
2019-10-30 16:43:59 +0100

    Cleanup SaltTestCase

--- Test/Cases/SaltTestCase.swift
+++ Test/Cases/SaltTestCase.swift
@@ -2,16 +2,18 @@ import CommonCrypto
 import XCTest
 
 class SaltTestCase: XCTestCase {
-    
+
+    override func tearDown() {
+        super.tearDown()
+
+        CCRandomGenerateBytesConfiguration.current = nil
+    }
+
     func testRandomNumberGeneratorFailure() {
         CCRandomGenerateBytesConfiguration.current = CCRandomGenerateBytesConfiguration.failure()
 
         XCTAssertThrowsError(try Salt(size: 0)) { error in
             XCTAssertEqual(error as? SaltError, SaltError.randomNumberGeneratorFailure)
-        }
-        
-        addTeardownBlock {
-            CCRandomGenerateBytesConfiguration.current = nil
         }
     }
 
@@ -21,24 +23,15 @@ class SaltTestCase: XCTestCase {
         try Salt(size: 0).withUnsafeBytes { salt in
             XCTAssertEqual(salt.count, 0)
         }
-
-        addTeardownBlock {
-            CCRandomGenerateBytesConfiguration.current = nil
-        }
     }
 
     func testEveryByteValue() throws {
-        let expectedBytes = Array(0 ..< UInt8.max)
+        let expectedBytes = Array(0 ... UInt8.max)
         CCRandomGenerateBytesConfiguration.current = CCRandomGenerateBytesConfiguration.withBytes(expectedBytes)
 
-        let saltSize = Int(UInt8.max)
-        try Salt(size: saltSize).withUnsafeBytes { salt in
+        try Salt(size: expectedBytes.count).withUnsafeBytes { salt in
             let resultBytes = Array(salt)
             XCTAssertEqual(resultBytes, expectedBytes)
-        }
-        
-        addTeardownBlock {
-            CCRandomGenerateBytesConfiguration.current = nil
         }
     }
     
