f35d3226b64391ca3cca6877df8c5af8b5920a8d
Johannes Auer <jmjauer@gmail.com>
2019-11-06 16:12:49 +0100

    Refactor key wrap/unwrap as KeyCryptor struct

---
+++ Common/Crypto/KeyCryptor.swift
@@ -0,0 +1,99 @@
+import CommonCrypto
+import CryptoKit
+import Foundation
+
+struct KeyCryptor {
+    
+    let keyEncryptionKey: SymmetricKey
+    
+    func wrap(_ key: SymmetricKey) throws -> Data {
+        var bufferSize = key.withUnsafeBytes { key in
+            return CCSymmetricWrappedSize(.rfc3394, key.count)
+        }
+        let buffer = UnsafeMutableBufferPointer<UInt8>.allocate(capacity: bufferSize)
+        buffer.initialize(repeating: 0)
+        defer {
+            buffer.assign(repeating: 0)
+            buffer.deinitialize()
+            buffer.deallocate()
+        }
+        
+        let status = key.withUnsafeBytesCopy { key in
+            return keyEncryptionKey.withUnsafeBytesCopy { keyEncryptionKey in
+                return CCSymmetricKeyWrap(.rfc3394, CCrfc3394_iv, CCrfc3394_ivLen, keyEncryptionKey.baseAddress, keyEncryptionKey.count, key.baseAddress, key.count, buffer.baseAddress, &bufferSize)
+            }
+        }
+        guard status == kCCSuccess else {
+            throw KeyCryptorError.keyWrapFailure
+        }
+        
+        return Data(buffer)
+    }
+    
+    func unwrap(_ wrappedKey: Data) throws -> SymmetricKey {
+        var bufferSize = CCSymmetricUnwrappedSize(.rfc3394, wrappedKey.count)
+        let buffer = UnsafeMutableBufferPointer<UInt8>.allocate(capacity: bufferSize)
+        buffer.initialize(repeating: 0)
+        defer {
+            buffer.assign(repeating: 0)
+            buffer.deinitialize()
+            buffer.deallocate()
+        }
+        
+        let status = wrappedKey.withUnsafeBytesCopy { key in
+            return keyEncryptionKey.withUnsafeBytesCopy { keyEncryptionKey in
+                return CCSymmetricKeyUnwrap(.rfc3394, CCrfc3394_iv, CCrfc3394_ivLen, keyEncryptionKey.baseAddress, keyEncryptionKey.count, key.baseAddress, key.count, buffer.baseAddress, &bufferSize)
+            }
+        }
+        guard status == kCCSuccess else {
+            throw KeyCryptorError.keyUnwrapFailure
+        }
+        
+        return SymmetricKey(data: buffer)
+    }
+    
+}
+
+enum KeyCryptorError: Error {
+    
+    case keyWrapFailure
+    case keyUnwrapFailure
+    
+}
+
+private extension CCWrappingAlgorithm {
+    
+    static let rfc3394 = CCWrappingAlgorithm(kCCWRAPAES)
+    
+}
+
+private extension UnsafeMutableBufferPointer {
+    
+    func deinitialize() {
+        self.baseAddress?.deinitialize(count: self.count)
+    }
+    
+}
+
+private extension ContiguousBytes {
+    
+    func withUnsafeBytesCopy<R>(_ body: (UnsafeBufferPointer<UInt8>) throws -> R) rethrows -> R {
+        return try self.withUnsafeBytes { rawBuffer in
+            let byteBuffer = UnsafeMutableBufferPointer<UInt8>.allocate(capacity: rawBuffer.count)
+            byteBuffer.initialize(repeating: 0)
+            defer {
+                byteBuffer.assign(repeating: 0)
+                byteBuffer.deinitialize()
+                byteBuffer.deallocate()
+            }
+            
+            for index in 0 ..< rawBuffer.count {
+                byteBuffer[index] = rawBuffer.load(fromByteOffset: index, as: UInt8.self)
+            }
+            
+            let readOnlyByteBuffer = UnsafeBufferPointer(byteBuffer)
+            return try body(readOnlyByteBuffer)
+        }
+    }
+    
+}
