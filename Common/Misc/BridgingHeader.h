d7abe9fda0b50f810b2fb1c2fbb822b742012664
Johannes Auer <jmjauer@gmail.com>
2019-12-30 12:30:25 +0100

    Simplify crypto C api

--- Common/Misc/BridgingHeader.h
+++ Common/Misc/BridgingHeader.h
@@ -1,4 +1,6 @@
 #import <CommonCrypto/CommonCrypto.h>
 
-int SymmetricKeyWrap(CCWrappingAlgorithm algorithm, const void *iv, const size_t ivLen, const void *kek, size_t kekLen, const void *rawKey, size_t rawKeyLen, void *wrappedKey,size_t *wrappedKeyLen);
-int SymmetricKeyUnwrap(CCWrappingAlgorithm algorithm, const void *iv, const size_t ivLen, const void *kek, size_t kekLen, const void *wrappedKey, size_t wrappedKeyLen, void *rawKey, size_t *rawKeyLen);
+int SymmetricKeyWrap(const void *kek, size_t kekLen, const void *rawKey, size_t rawKeyLen, void *wrappedKey, size_t wrappedKeyLen);
+int SymmetricKeyUnwrap(const void *kek, size_t kekLen, const void *wrappedKey, size_t wrappedKeyLen, void *rawKey, size_t rawKeyLen);
+size_t SymmetricWrappedSize(size_t rawKeyLen);
+size_t SymmetricUnwrappedSize(size_t wrappedKeyLen);
