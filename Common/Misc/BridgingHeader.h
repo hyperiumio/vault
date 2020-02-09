58d299b63867b6a02afcb8254fd7d02d698459f2
Johannes Auer <jmjauer@gmail.com>
2020-02-09 18:50:11 +0100

    Improve crypto C api

--- Common/Misc/BridgingHeader.h
+++ Common/Misc/BridgingHeader.h
@@ -1,7 +1,10 @@
-#import <CommonCrypto/CommonCrypto.h>
+#import <stddef.h>
 
+const int CryptoSuccess;
+const int CryptoFailure;
 int SymmetricKeyWrap(const void *kek, size_t kekLen, const void *rawKey, size_t rawKeyLen, void *wrappedKey, size_t wrappedKeyLen);
 int SymmetricKeyUnwrap(const void *kek, size_t kekLen, const void *wrappedKey, size_t wrappedKeyLen, void *rawKey, size_t rawKeyLen);
 size_t SymmetricWrappedSize(size_t rawKeyLen);
 size_t SymmetricUnwrappedSize(size_t wrappedKeyLen);
-int KeyDerivationn(const char *password, size_t passwordLen, const void *salt, size_t saltLen, unsigned int rounds, void *derivedKey, size_t derivedKeyLen);
+int DerivedKey(const char *password, size_t passwordLen, const void *salt, size_t saltLen, unsigned int rounds, void *derivedKey, size_t derivedKeyLen);
+int RandomBytes(void *bytes, size_t count);
--- Test/Doubles/SymmetricKeyWrapMock.swift
+++ Test/Doubles/SymmetricKeyWrapMock.swift
@@ -1,5 +1,6 @@
 import CryptoKit
 import Foundation
+import CommonCrypto
 
 class SymmetricKeyWrapMock {
     
