957a2c7158a072959f19d0bfd9766309aaead44e
Johannes Auer <jmjauer@gmail.com>
2020-04-02 21:58:56 +0200

    Add ContentWindowController

--- macOS/Controller/ApplicationController.swift
+++ macOS/Controller/ApplicationController.swift
@@ -2,4 +2,10 @@ import AppKit
 
 class ApplicationController: NSObject, NSApplicationDelegate {
     
+    let contentWindowController = ContentWindowController()
+    
+    func applicationDidFinishLaunching(_ notification: Notification) {
+        contentWindowController.showWindow()
+    }
+    
 }
---
+++ macOS/Controller/ContentWindowController.swift
@@ -0,0 +1,51 @@
+import AppKit
+import Combine
+import SwiftUI
+
+class ContentWindowController: NSObject {
+    
+    private let window: NSWindow
+    
+    override init() {
+        self.window = NSWindow(contentRect: .zero, styleMask: .applicationWindow, backing: .buffered, defer: false)
+        
+        super.init()
+        
+        window.titleVisibility = .hidden
+        window.isMovableByWindowBackground = true
+        window.center()
+        window.setFrameAutosaveName(.applicationWindowFrameAutosaveName)
+        window.delegate = self
+    }
+    
+    func showWindow() {
+        window.makeKeyAndOrderFront(nil)
+    }
+    
+}
+
+extension ContentWindowController: NSWindowDelegate {
+    
+    func windowWillClose(_ notification: Notification) {
+        NSApplication.shared.terminate(self)
+    }
+    
+}
+
+private extension NSWindow.StyleMask {
+    
+    static let applicationWindow: Self = [
+        .titled,
+        .closable,
+        .miniaturizable,
+        .resizable,
+        .fullSizeContentView
+    ]
+    
+}
+
+private extension String {
+    
+    static let applicationWindowFrameAutosaveName = "Application Window"
+    
+}
