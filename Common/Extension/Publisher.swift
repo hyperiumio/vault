38c17c7dab3f500f741f7bd85d7330eaee6599a6
Johannes Auer <jmjauer@gmail.com>
2020-04-13 17:24:01 +0200

    Add Publisher extension

---
+++ Common/Extension/Publisher.swift
@@ -0,0 +1,15 @@
+import Combine
+
+extension Publisher {
+    
+    func result() -> AnyPublisher<Result<Self.Output, Self.Failure>, Never> {
+        return self
+            .map { value in
+                return Result.success(value)
+            }
+            .catch { error in
+                return Just(Result.failure(error))
+            }
+            .eraseToAnyPublisher()
+    }
+}
