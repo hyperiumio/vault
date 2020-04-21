02f0006a58cab7cd387deac4d3f380eaf691fe26
Johannes Auer <jmjauer@gmail.com>
2020-04-21 17:57:06 +0200

    Minor

--- Common/Publisher/FileExistsPublisher.swift
+++ Common/Publisher/FileExistsPublisher.swift
@@ -4,8 +4,8 @@ import Foundation
 func FileExistsPublisher(url: URL) -> AnyPublisher<Bool, Never> {
     return Future { promise in
         DispatchQueue.global().async {
-            let keyExists = FileManager.default.fileExists(atPath: url.path)
-            let result = Result<Bool, Never>.success(keyExists)
+            let fileExists = FileManager.default.fileExists(atPath: url.path)
+            let result = Result<Bool, Never>.success(fileExists)
             promise(result)
         }
     }.eraseToAnyPublisher()
