10aaffeb5630b043df5e1ec313c16ac8ffe76c56
Johannes Auer <jmjauer@gmail.com>
2020-04-21 15:41:56 +0200

    Minor renaming

--- Common/Crypto/VaultItemCryptor.swift
+++ Common/Crypto/VaultItemCryptor.swift
@@ -6,19 +6,19 @@ struct VaultItemCryptor {
     let masterKey: SymmetricKey
     
     func encode(_ vaultItem: VaultItem) throws -> Data {
-        let encodedInfo = try VaultItemInfoEncode(vaultItem.info)
-        let encodedItems = try vaultItem.secureItems.map { vaultItem in
+        let encodedVaultItemInfo = try VaultItemInfoEncode(vaultItem.info)
+        let encodedSecureItems = try vaultItem.secureItems.map { vaultItem in
             return try SecureContainerItemEncode(vaultItem)
         }
-        let messages = [encodedInfo] + encodedItems
+        let messages = [encodedVaultItemInfo] + encodedSecureItems
         
         return try SecureData.encode(messages: messages, using: masterKey)
     }
     
     func decodeInfo(from context: ByteBufferContext) throws -> VaultItem.Info {
         let secureData = try SecureData(using: masterKey, from: context)
-        let infoData = try secureData.plaintext(at: .infoIndex, from: context)
-        return try VaultItemInfoDecode(data: infoData)
+        let encodedVaultItemInfo = try secureData.plaintext(at: .infoIndex, from: context)
+        return try VaultItemInfoDecode(data: encodedVaultItemInfo)
     }
     
 }
