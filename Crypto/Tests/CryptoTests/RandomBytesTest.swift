77e89155a0e2bc900d039f71eaa47eb1796f604b
Johannes Auer <jmjauer@gmail.com>
2020-05-20 10:47:26 +0200

    Add allocation test for RandomBytes

--- Crypto/Tests/CryptoTests/RandomBytesTest.swift
+++ Crypto/Tests/CryptoTests/RandomBytesTest.swift
@@ -5,13 +5,23 @@ import XCTest
 final class RandomBytesTest: XCTestCase {
     
     func testZeroCount() throws {
-        let randomData = try RandomBytes(count: 0)
+        let expectedByteCount = 0
         
-        XCTAssertEqual(randomData.count, 0)
+        func allocator(byteCount: Int, alignment: Int) -> UnsafeMutableRawPointer {
+            XCTAssertEqual(byteCount, expectedByteCount)
+            XCTAssertEqual(alignment, MemoryLayout<UInt8>.alignment)
+            
+            return UnsafeMutableRawPointer.allocate(byteCount: byteCount, alignment: alignment)
+        }
+        
+        let randomData = try RandomBytes(count: expectedByteCount, allocator: allocator)
+        
+        XCTAssertEqual(randomData.count, expectedByteCount)
     }
     
     func testReturnsRandomBytes() throws {
         let expectedData = Data(0 ... UInt8.max)
+        let buffer = UnsafeMutableRawPointer.allocate(byteCount: expectedData.count, alignment: MemoryLayout<UInt8>.alignment)
         
         func rng(buffer: UnsafeMutableRawPointer?, count: Int) -> Int32 {
             expectedData.withUnsafeBytes { bytes in
@@ -21,9 +31,20 @@ final class RandomBytesTest: XCTestCase {
             return Int32(kCCSuccess)
         }
         
-        let randomData = try RandomBytes(count: expectedData.count, rng: rng)
+        func allocator(byteCount: Int, alignment: Int) -> UnsafeMutableRawPointer {
+            XCTAssertEqual(byteCount, expectedData.count)
+            XCTAssertEqual(alignment, MemoryLayout<UInt8>.alignment)
+            
+            return buffer
+        }
+        
+        let randomData = try RandomBytes(count: expectedData.count, rng: rng, allocator: allocator)
         
         XCTAssertEqual(randomData, expectedData)
+        for (index, expectedByte) in expectedData.enumerated() {
+            let byte = buffer.load(fromByteOffset: index, as: UInt8.self)
+            XCTAssertEqual(byte, expectedByte)
+        }
     }
     
     func testFailure() {
