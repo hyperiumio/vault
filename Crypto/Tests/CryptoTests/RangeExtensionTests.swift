934d39f148e8d45ef2bc77e9e7c0c4faa897ac62
Johannes Auer <jmjauer@gmail.com>
2020-05-20 08:54:00 +0200

    Add RangeExtension tests

---
+++ Crypto/Tests/CryptoTests/RangeExtensionTests.swift
@@ -0,0 +1,19 @@
+import XCTest
+@testable import Crypto
+
+final class RangeExtensionTests: XCTestCase {
+    
+    func testZeroCount() {
+        let range = Range(lowerBound: 0, count: 0)
+        
+        XCTAssertEqual(range.count, 0)
+    }
+    
+    func testLowerBoundAndUpperbound() {
+        let range = Range(lowerBound: 1, count: 2)
+        
+        XCTAssertEqual(range.lowerBound, 1)
+        XCTAssertEqual(range.upperBound, 3)
+    }
+    
+}
