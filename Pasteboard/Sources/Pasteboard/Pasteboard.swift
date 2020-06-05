90225511152b804cf4bfcca23fe0003a24e6e70a
Johannes Auer <jmjauer@gmail.com>
2020-06-05 14:39:51 +0200

    Add Pasteboard package

---
+++ Pasteboard/Sources/Pasteboard/Pasteboard.swift
@@ -0,0 +1,56 @@
+#if canImport(AppKit)
+import AppKit
+
+public class Pasteboard {
+    
+    private let systemPasteboard: NSPasteboard
+    
+    public var string: String? {
+        get {
+            return systemPasteboard.string(forType: .string)
+        }
+        set(string) {
+            systemPasteboard.clearContents()
+            
+            guard let string = string else {
+                return
+            }
+            systemPasteboard.setString(string, forType: .string)
+        }
+    }
+    
+    private init(systemPasteboard: NSPasteboard) {
+        self.systemPasteboard = systemPasteboard
+    }
+    
+}
+#endif
+
+#if canImport(UIKit)
+import UIKit
+
+public class Pasteboard {
+    
+    private let systemPasteboard: UIPasteboard
+    
+    public var string: String? {
+        get {
+            return systemPasteboard.string
+        }
+        set(string) {
+            systemPasteboard.string = string
+        }
+    }
+    
+    private init(systemPasteboard: UIPasteboard) {
+        self.systemPasteboard = systemPasteboard
+    }
+    
+}
+#endif
+
+extension Pasteboard {
+    
+    public static let general = Pasteboard(systemPasteboard: .general)
+    
+}
---
+++ Pasteboard/Tests/PasteboardTests/PasteboardTests.swift
@@ -0,0 +1,4 @@
+import XCTest
+@testable import Pasteboard
+
+final class PasteboardTests: XCTestCase {}
