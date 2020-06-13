9e08c7b5e9da6417d67b25947f1d2f47288b2f15
Johannes Auer <jmjauer@gmail.com>
2020-06-13 21:00:59 +0200

    Delete obsolete code

--- Common/Extension/ResultExtension.swift
+++ Common/Extension/ResultExtension.swift
@@ -1,11 +1,3 @@
-extension Result where Success == Void {
-    
-    static var success: Self {
-        return Self.success(())
-    }
-    
-}
-
 extension Result where Failure == Never {
     
     static func success(value: Success) -> Self {
