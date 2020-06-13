5c5bde609b03ee36032f05bcbecd1ef4c5870e6b
Johannes Auer <jmjauer@gmail.com>
2020-06-13 21:00:59 +0200

    Delete obsolete code

--- Common/Extension/DataExtension.swift
+++ Common/Extension/DataExtension.swift
@@ -2,34 +2,10 @@ import Foundation
 
 extension Data {
     
-    static let empty = Self()
-    
     func map<T>(_ transform: (Self) throws -> T) rethrows -> T {
         return try transform(self)
     }
     
 }
 
-extension Data: ExpressibleByIntegerLiteral {
 
-    public init(integerLiteral value: UInt8) {
-        let bytes = [value]
-        self = Data(bytes)
-    }
-
-}
-
-extension Data: ExpressibleByStringLiteral {
-
-    public init(stringLiteral value: String) {
-        let bytes = stride(from: 0, to: value.count, by: 2).map { hexDigestIndex in
-            let start = value.index(value.startIndex, offsetBy: hexDigestIndex)
-            let end = value.index(start, offsetBy: 2, limitedBy: value.endIndex) ?? value.endIndex
-            let hexDigest = value[start ..< end]
-            return UInt8(hexDigest, radix: 16)!
-        } as [UInt8]
-
-        self = Data(bytes)
-    }
-
-}
