c01c4ec402f694bf6446d9bb88f24acc93232ed8
Johannes Auer <jmjauer@gmail.com>
2020-12-15 17:25:44 +0100

    Add CryptoKey tests

--- Crypto/Tests/CryptoKeyTests.swift
+++ Crypto/Tests/CryptoKeyTests.swift
@@ -1,363 +1,167 @@
+import CommonCrypto
+import CryptoKit
 import XCTest
 @testable import Crypto
 
 class CryptoKeyTests: XCTestCase {
-}
-
-/*
-class MasterKeyContainerTests: XCTestCase {
-    
-    override func setUp() {
-        super.setUp()
-        
-        MasterKeyContainerRandomBytes = { _ in fatalError() }
-        MasterKeyContainerDerivedKey = { _, _, _ ,_ in fatalError() }
-        MasterKeyContainerSeal = { _, _, _ in fatalError() }
-        MasterKeyContainerOpen = { _, _ in fatalError() }
-    }
-    
-    override func tearDown() {
-        MasterKeyContainerRandomBytes = RandomBytes
-        MasterKeyContainerDerivedKey = DerivedKey
-        MasterKeyContainerSeal = AES.GCM.seal
-        MasterKeyContainerOpen = AES.GCM.open
-        
-        super.tearDown()
-    }
-    
-    func testEncodeSuccess() throws {
-        let expectedSalt = Data(0 ..< 32)
-        let expectedNonce = Data(32 ..< 44)
-        let expectedCiphertext = Data(44 ..< 76)
-        let expectedTag = Data(76 ..< 92)
-        
-        MasterKeyContainerRandomBytes = { _ in expectedSalt }
-        MasterKeyContainerDerivedKey = { _, _, _ ,_ in SymmetricKey(size: .bits256) }
-        
-        MasterKeyContainerSeal = { _, _, _ in
-            let expectedNonce = try AES.GCM.Nonce(data: expectedNonce)
-            return try AES.GCM.SealedBox(nonce: expectedNonce, ciphertext: expectedCiphertext, tag: expectedTag)
-        }
-        
-        let masterKey = MasterKey()
-        let result = try MasterKeyContainerEncode(masterKey, with: "foo")
-        
-        let versionRange = Range(lowerBound: result.startIndex, count: 1)
-        let saltRange = Range(lowerBound: versionRange.upperBound, count: expectedSalt.count)
-        let roundsRange = Range(lowerBound: saltRange.upperBound, count: UnsignedInteger32BitEncodingSize)
-        let nonceRange = Range(lowerBound: roundsRange.upperBound, count: expectedNonce.count)
-        let ciphertextRange = Range(lowerBound: nonceRange.upperBound, count: expectedCiphertext.count)
-        let tagRange = Range(lowerBound: ciphertextRange.upperBound, count: expectedTag.count)
-        
-        let version = result[versionRange]
-        let salt = result[saltRange]
-        let rounds = result[roundsRange]
-        let nonce = result[nonceRange]
-        let ciphertext = result[ciphertextRange]
-        let tag = result[tagRange]
-        
-        XCTAssertEqual(version, "01")
-        XCTAssertEqual(salt, expectedSalt)
-        XCTAssertEqual("00000800", rounds)
-        XCTAssertEqual(nonce, expectedNonce)
-        XCTAssertEqual(ciphertext, expectedCiphertext)
-        XCTAssertEqual(tag, expectedTag)
-    }
-    
-    func testEncodeRandomBytesArguments() throws {
-        MasterKeyContainerRandomBytes = { count in
-            XCTAssertEqual(count, 32)
-            
-            return Data(repeating: 0, count: count)
-        }
-        
-        MasterKeyContainerDerivedKey = { _, _, _, _ in SymmetricKey(size: .bits256) }
-        
-        MasterKeyContainerSeal = { _, _, _ in
-            let combined = Data(repeating: 0, count: 28)
-            return try AES.GCM.SealedBox(combined: combined)
-        }
-        
-        let masterKey = MasterKey()
-        _ = try MasterKeyContainerEncode(masterKey, with: "")
-    }
-    
-    func testEncodeDerivedKeyArguments() throws {
-        let expectedSalt = Data(0 ..< 32)
-        let expectedPassword = "foo"
-        
-        MasterKeyContainerRandomBytes = { _ in expectedSalt }
-        
-        MasterKeyContainerDerivedKey = { salt, rounds, keySize, password in
-            XCTAssertEqual(salt, expectedSalt)
-            XCTAssertEqual(rounds, 524288)
-            XCTAssertEqual(keySize, 32)
-            XCTAssertEqual(password, expectedPassword)
-            
-            return SymmetricKey(size: .bits256)
-        }
-        
-        MasterKeyContainerSeal = { _, _, _ in
-            let combined = Data(repeating: 0, count: 28)
-            return try AES.GCM.SealedBox(combined: combined)
-        }
-        
-        let masterKey = MasterKey()
-        _ = try MasterKeyContainerEncode(masterKey, with: expectedPassword)
-    }
-    
-    func testEncodeEncryptionArguments() throws {
-        let expectedDerivedKey = Data(0 ..< 32)
-        let expectedCryptoKey = "FF" as Data
-        
-        MasterKeyContainerRandomBytes = { _ in Data(repeating: 0, count: 32) }
-        MasterKeyContainerDerivedKey = { _, _, _, _ in SymmetricKey(data: expectedDerivedKey) }
-        
-        MasterKeyContainerSeal = { cryptoKey, derivedKey, nonce in
-            let cryptoKey = Data(cryptoKey)
-            let derivedKey = derivedKey.withUnsafeBytes { derivedKey in
-                return Data(derivedKey)
-            }
-            
-            XCTAssertEqual(cryptoKey, expectedCryptoKey)
-            XCTAssertEqual(derivedKey, expectedDerivedKey)
-            XCTAssertNil(nonce)
-            
-            let combined = Data(repeating: 0, count: 28)
-            return try AES.GCM.SealedBox(combined: combined)
-        }
-        
-        let masterKey = MasterKey(expectedCryptoKey)
-        _ = try MasterKeyContainerEncode(masterKey, with: "")
-    }
-    
-    func testEncodeRandomNumberGeneratorFailure() {
-        MasterKeyContainerRandomBytes = { _ in throw RandomBytesError.rngFailure }
-        
-        let masterKey = MasterKey()
-              
-        XCTAssertThrowsError(try MasterKeyContainerEncode(masterKey, with: ""))
-    }
-    
-    func testEncodeDerivedKeyFailure() {
-        MasterKeyContainerRandomBytes = { _ in Data(repeating: 0, count: 32) }
-        MasterKeyContainerDerivedKey = { _, _, _, _ in throw DerivedKeyError.keyDerivationFailure }
-        
-        let masterKey = MasterKey()
-              
-        XCTAssertThrowsError(try MasterKeyContainerEncode(masterKey, with: ""))
-    }
-    
-    func testEncodeEncryptionFailure() {
-        MasterKeyContainerRandomBytes = { _ in Data(repeating: 0, count: 32) }
-        MasterKeyContainerDerivedKey = { _, _, _, _ in SymmetricKey(size: .bits256) }
-        MasterKeyContainerSeal = { _, _, _ in throw NSError() }
-        
-        let masterKey = MasterKey()
-              
-        XCTAssertThrowsError(try MasterKeyContainerEncode(masterKey, with: ""))
-    }
-    
-    func testDecodeSuccess() throws {
-        let data = Data("01") + Data(repeating: 0, count: 96)
-        let expectedMasterKey = Data(96 ..< 128)
-        
-        MasterKeyContainerDerivedKey = { _, _, _, _ in SymmetricKey(size: .bits256) }
-        MasterKeyContainerOpen = { _, _ in expectedMasterKey }
-        
-        let masterKey = try MasterKeyContainerDecode(data, with: "").cryptoKey.withUnsafeBytes { bytes in Data(bytes) }
-        
-        XCTAssertEqual(masterKey, expectedMasterKey)
-    }
-    
-    func testDecodeIsDataSliceIndependent() throws {
-        let version = Data("01")
-        let expectedSalt = Data(2 ..< 34)
-        let rounds = Data(34 ..< 38)
-        let expectedWrappedKey = Data(38 ..< 98)
-        let dataChunk = Data("FF") + version + expectedSalt + rounds + expectedWrappedKey
-        let containerData = dataChunk[1...]
-        
-        MasterKeyContainerDerivedKey = { salt, rounds, _, _ in
-            XCTAssertEqual(salt, expectedSalt)
-            XCTAssertEqual(rounds, 623125282)
-            
-            return SymmetricKey(size: .bits256)
-        }
-        
-        MasterKeyContainerOpen = { wrappedKey, _ in
-            XCTAssertEqual(wrappedKey.combined, expectedWrappedKey)
-            
-            return Data(repeating: 0, count: 32)
-        }
-        
-        _ = try MasterKeyContainerDecode(containerData, with: "")
-    }
-    
-    func testDecodeVersion1DerivedKeyArguments() throws {
-        let expectedPassword = "foo"
-        let version = Data("01")
-        let expectedSalt = Data(2 ..< 34)
-        let rounds = Data(34 ..< 38)
-        let wrappedKey = Data(repeating: 0, count: 60)
-        let data = version + expectedSalt + rounds + wrappedKey
-        
-        MasterKeyContainerDerivedKey = { salt, rounds, keySize, password in
-            XCTAssertEqual(salt, expectedSalt)
-            XCTAssertEqual(rounds, 623125282)
-            XCTAssertEqual(keySize, 32)
-            XCTAssertEqual(password, expectedPassword)
-            
-            return SymmetricKey(size: .bits256)
-        }
-        
-        MasterKeyContainerOpen = { _, _ in
-            return Data(repeating: 0, count: 32)
-        }
-        
-        _ = try MasterKeyContainerDecode(data, with: expectedPassword)
-    }
-    
-    func testDecodeVersion1DecryptArguments() throws {
-        let version = Data("01")
-        let salt = Data(repeating: 0, count: 32)
-        let rounds = Data(repeating: 0, count: 4)
-        let expectedWrappedKey = Data(0 ..< 60)
-        let data = version + salt + rounds + expectedWrappedKey
-        let expectedDerivedKey = Data(60 ..< 92)
-        
-        MasterKeyContainerDerivedKey = { _, _, _, _ in
-            return SymmetricKey(data: expectedDerivedKey)
-        }
-        
-        MasterKeyContainerOpen = { wrappedKey, derivedKey in
-            let derivedKey = derivedKey.withUnsafeBytes { derivedKey in Data(derivedKey) }
-            
-            XCTAssertEqual(wrappedKey.combined, expectedWrappedKey)
-            XCTAssertEqual(derivedKey, expectedDerivedKey)
-            
-            return Data(repeating: 0, count: 32)
-        }
-        
-        _ = try MasterKeyContainerDecode(data, with: "")
-    }
-    
-    func testDecodeInvalidDataSize() {
-        XCTAssertThrowsError(try MasterKeyContainerDecode(.empty, with: ""))
-    }
-    
-    func testDecodeInvalidVersion() {
-        XCTAssertThrowsError(try MasterKeyContainerDecode("02", with: ""))
-    }
-    
-    func testDecodeVersion1InvalidDataSize() {
-        XCTAssertThrowsError(try MasterKeyContainerDecode("0100", with: ""))
-    }
-    
-    func testDecodeVersion1DerivedKeyFailure() {
-        let data = Data("01") + Data(0 ..< 96)
-        
-        MasterKeyContainerDerivedKey = { _, _, _, _ in throw DerivedKeyError.keyDerivationFailure }
-        
-        XCTAssertThrowsError(try MasterKeyContainerDecode(data, with: ""))
-    }
-    
-    func testDecodeDecryptionFailure() {
-        let data = Data("01") + Data(0 ..< 96)
-        
-        MasterKeyContainerDerivedKey = { _, _, _, _ in SymmetricKey(size: .bits256) }
-        MasterKeyContainerOpen = { _, _ in throw NSError() }
-        
-        XCTAssertThrowsError(try MasterKeyContainerDecode(data, with: ""))
-    }
-    
-}
-*/
-
-/*
-class MasterKeyTests: XCTestCase {
     
     func testInit() {
-        let masterKey = MasterKey()
+        let key = CryptoKey()
         
-        XCTAssertEqual(masterKey.cryptoKey.bitCount, 256)
+        XCTAssertEqual(key.value.bitCount, SymmetricKeySize.bits256.bitCount)
     }
     
-    func testInitWithData() {
-        let expectedData = Data(0 ..< 32)
+    func testInitFromKey() {
+        let rawKey = SymmetricKey(size: .bits256)
+        let key = CryptoKey(rawKey)
         
-        let cryptoKeyData = MasterKey(expectedData).cryptoKey.withUnsafeBytes { cryptoKey in
-            return Data(cryptoKey)
+        XCTAssertEqual(key.value, rawKey)
+    }
+    
+    func testInitFromData() {
+        let keyData = Data(0 ... UInt8.max)
+        let rawKey = SymmetricKey(data: keyData)
+        let key = CryptoKey(keyData)
+        
+        XCTAssertEqual(key.value, rawKey)
+    }
+    
+    func testInitFromDerivedKeyContainerSuccess() throws {
+        let prefix = [UInt8.max]
+        let salt = Array(repeating: 0, count: 32) as [UInt8]
+        let rounds = [1, 0, 0, 0]  as [UInt8]
+        let container = Data(prefix + salt + rounds)[1...]
+        
+        let key = try CryptoKey(fromDerivedKeyContainer: container, password: "").withUnsafeBytes { bytes in
+            Array(bytes)
         }
         
-        XCTAssertEqual(cryptoKeyData, expectedData)
+        let expectedKey = [160, 172, 145, 150, 69, 134, 235, 176, 226, 167, 116, 34, 123, 31, 108, 208, 49, 231, 71, 212, 129, 55, 35, 165, 226, 33, 76, 245, 0, 51, 19, 216] as [UInt8]
+        
+        XCTAssertEqual(key, expectedKey)
+    }
+    
+    func testInitFromDerivedKeyContainerInvalidContainerSize() {
+        let container = Data()
+        
+        XCTAssertThrowsError(try CryptoKey(fromDerivedKeyContainer: container, password: ""))
+    }
+    
+    func testInitFromDerivedKeyContainerKeyDerivationFailure() {
+        let container = Data(repeating: 0, count: 36)
+        
+        XCTAssertThrowsError(try CryptoKey(fromDerivedKeyContainer: container, password: ""))
+    }
+    
+    func testInitFromEncryptedKeyContainerSuccess() throws {
+        let prefix = [0] as [UInt8]
+        let containerBytes = [133, 208, 73, 193, 145, 236, 235, 37, 246, 167, 22, 202, 87, 154, 38, 81, 253, 82, 20, 195, 48, 163, 38, 186, 98, 12, 5, 231, 155, 91, 35, 114, 206, 213, 130, 114, 117, 174, 133, 84, 89, 194, 130, 181, 178, 157, 173, 5, 36, 147, 6, 153, 177, 243, 167, 2, 176, 122, 128, 84] as [UInt8]
+        let container = Data(prefix + containerBytes)[1...]
+        
+        let derivedKeyData = Data(repeating: 0, count: 32)
+        let derivedKey = CryptoKey(derivedKeyData)
+        
+        let masterKey = try CryptoKey(fromEncryptedKeyContainer: container, using: derivedKey).withUnsafeBytes { bytes in
+            Array(bytes)
+        }
+        let expectedKey = Array(repeating: UInt8.max, count: 32)
+        
+        XCTAssertEqual(masterKey, expectedKey)
+    }
+    
+    func testInitFromEncryptedKeyContainerInvalidContainer() {
+        let container = Data()
+        let derivedKeyData = Data(repeating: 0, count: 32)
+        let derivedKey = CryptoKey(derivedKeyData)
+        
+        XCTAssertThrowsError(try CryptoKey(fromEncryptedKeyContainer: container, using: derivedKey))
+    }
+    
+    func testInitFromEncryptedKeyContainerDecryptionFailure() {
+        let containerBytes = [133, 208, 73, 193, 145, 236, 235, 37, 246, 167, 22, 202, 87, 154, 38, 81, 253, 82, 20, 195, 48, 163, 38, 186, 98, 12, 5, 231, 155, 91, 35, 114, 206, 213, 130, 114, 117, 174, 133, 84, 89, 194, 130, 181, 178, 157, 173, 5, 36, 147, 6, 153, 177, 243, 167, 2, 176, 122, 128, 84] as [UInt8]
+        let container = Data(containerBytes)
+        
+        let derivedKeyData = Data(repeating: UInt8.max, count: 32)
+        let derivedKey = CryptoKey(derivedKeyData)
+        
+        XCTAssertThrowsError(try CryptoKey(fromEncryptedKeyContainer: container, using: derivedKey))
+    }
+    
+    func testEncryptedKeyContainerSuccess() throws {
+        let derivedKeyData = Data(repeating: 0, count: 32)
+        let derivedKey = CryptoKey(derivedKeyData)
+        let masterKeyData = Data(repeating: UInt8.max, count: 32)
+        let masterKey = CryptoKey(masterKeyData)
+        
+        let container = try masterKey.encryptedKeyContainer(using: derivedKey)
+        
+        XCTAssertEqual(container.count, 60)
+    }
+    
+    func testEncryptedKeyContainerEncryptionFailure() {
+        let keyData = Data()
+        let derivedKey = CryptoKey(keyData)
+        let masterKey = CryptoKey(keyData)
+        
+        XCTAssertThrowsError(try masterKey.encryptedKeyContainer(using: derivedKey))
+    }
+    
+    func testWithUnsafeBytes() {
+        let keyData = Data(0 ... UInt8.max)
+        let bytes = CryptoKey(keyData).withUnsafeBytes { bytes in
+            Data(bytes)
+        }
+        
+        XCTAssertEqual(keyData, bytes)
+    }
+    
+    func testDerivedKeyContainerSize() {
+        XCTAssertEqual(CryptoKey.derivedKeyContainerSize, 36)
+    }
+    
+    func testEncryptedKeyContainerSize() {
+        XCTAssertEqual(CryptoKey.encryptedKeyContainerSize, 60)
+    }
+    
+    func testDeriveFromPasswordSuccess() throws {
+        let derived = try CryptoKey.derive(from: "")
+        
+        XCTAssertEqual(derived.container.count, 36)
+        XCTAssertEqual(derived.key.value.bitCount, SymmetricKeySize.bits256.bitCount)
+    }
+    
+    func testDeriveFromPasswordRNGFailure() {
+        let random = { _, _ in
+            CCRNGStatus(kCCRNGFailure)
+        } as CryptoKey.Configuration.Random
+        let configuration = CryptoKey.Configuration.stub(random: random)
+        
+        XCTAssertThrowsError(try CryptoKey.derive(from: "", configuration: configuration))
+    }
+
+    func testDeriveFromPasswordKeyDerivationFailure() {
+        let kdf = { _, _, _, _, _, _, _, _, _ in
+            Int32(kCCUnimplemented)
+        } as CryptoKey.Configuration.KDF
+        let configuration = CryptoKey.Configuration.stub(kdf: kdf)
+        
+        XCTAssertThrowsError(try CryptoKey.derive(from: "", configuration: configuration))
     }
     
 }
-*/
 
-
-/*
-class DerivedKeyTests: XCTestCase {
+private extension CryptoKey.Configuration {
     
-    override func setUp() {
-        super.setUp()
-        
-        DerivedKeyKDF = { _, _, _, _, _, _, _ in fatalError() }
+    static func random(bytes: UnsafeMutableRawPointer?, _ count: Int) -> CCRNGStatus {
+        CCRNGStatus(kCCSuccess)
     }
     
-    override func tearDown() {
-        DerivedKeyKDF = CoreCrypto.DerivedKey
-        
-        super.tearDown()
+    static func kdf(algorithm: CCPBKDFAlgorithm, password: UnsafePointer<Int8>, passwordLen: Int, salt: UnsafePointer<UInt8>, saltLen: Int, prf: CCPseudoRandomAlgorithm, rounds: UInt32, derivedKey: UnsafeMutablePointer<UInt8>, derivedKeyLen: Int) -> Int32 {
+        Int32(kCCSuccess)
     }
     
-    func testDerivedKeySuccess() throws {
-        let expectedKeyData = Data(0 ..< 32)
-        
-        DerivedKeyKDF = { _, _, _, _, _, buffer, _ in
-            expectedKeyData.withUnsafeBytes { expectedKeyData in
-                buffer?.copyMemory(from: expectedKeyData.baseAddress!, byteCount: expectedKeyData.count)
-            }
-            
-            return CoreCrypto.Success
-        }
-        
-        let derivedKeyData = try DerivedKey(salt: .empty, rounds: 0, keySize: expectedKeyData.count, password: "").withUnsafeBytes { key in
-            return Data(key)
-        }
-        
-        XCTAssertEqual(derivedKeyData, expectedKeyData)
-    }
-    
-    func testKDFArguments() throws {
-        let expectedPassword = "foo"
-        let expectedPasswordData = Data(expectedPassword.utf8)
-        let expectedSalt = Data(0 ..< 32)
-        let expectedRounds = UInt32(524288)
-        let expectedKeySize = 32
-        
-        DerivedKeyKDF = { password, passwordSize, salt, saltSize, rounds, derivedKey, derivedKeySize in
-            let password = Data(bytes: password!, count: passwordSize)
-            let salt = Data(bytes: salt!, count: saltSize)
-            
-            XCTAssertEqual(password, expectedPasswordData)
-            XCTAssertEqual(salt, expectedSalt)
-            XCTAssertEqual(rounds, expectedRounds)
-            XCTAssertNotNil(derivedKey)
-            XCTAssertEqual(derivedKeySize, expectedKeySize)
-            
-            return CoreCrypto.Success
-        }
-        
-        _ = try DerivedKey(salt: expectedSalt, rounds: expectedRounds, keySize: expectedKeySize, password: expectedPassword)
-    }
-    
-    func testKeyDerivationFailure() {
-        DerivedKeyKDF = { _, _, _, _, _, _, _ in -1 }
-        
-        XCTAssertThrowsError(try DerivedKey(salt: .empty, rounds: 0, keySize: 0, password: ""))
+    static func stub(random: @escaping Random = random, kdf: @escaping KDF = kdf) -> Self {
+        Self(random: random, kdf: kdf)
     }
     
 }
-*/
